67755bb955447f480f62835f7055fec55fb9424d
TheTofuMan <jroyzen07@gmail.com>
2024-03-05 18:03:01 -0800

    [style] change to 2 space tab

--- src/main/java/frc/robot/commands/AutoAlign.java
+++ src/main/java/frc/robot/commands/AutoAlign.java
@@ -13,53 +13,51 @@ import frc.robot.subsystems.Drivetrain;
 // https://docs.limelightvision.io/docs/docs-limelight/apis/limelight-lib#pose-estimation
 // https://docs.limelightvision.io/docs/docs-limelight/pipeline-apriltag/apriltag-robot-localization#using-wpilibs-pose-estimator
 
-// TODO(limelight): extend CommandBase. Also, literally everything else is in 2-space indents. Change it. Finally, get rid of all of these random stub methods. (ignore this and don't use this file, leaving it here just for feedback)
 public class AutoAlign extends Command{
-    private static Limelight limelight;
+  private static Limelight limelight;
 
+  @Override
+  public void initialize() {
+      super.initialize();
+  }
 
-    @Override
-    public void initialize() {
-        super.initialize();
-    }
+  public static double[] getDistanceFromAngle(Limelight limelight){
+      double yangle = limelight.getTy();
+      double xangle = limelight.getTx();
+      double yangleradians = (yangle + DeviceConsts.limelightAngle) * (3.14 / 180);
+      double ydistance = FieldConsts.speakerAprilTagY - DeviceConsts.limelightHeight /Math.tan(yangleradians);
+      double xdistance = ydistance/Math.tan(xangle);
+      return new double[] {xdistance, ydistance};
+  }
 
-    public static double[] getDistanceFromAngle(Limelight limelight){
-        double yangle = limelight.getTy();
-        double xangle = limelight.getTx();
-        double yangleradians = (yangle + DeviceConsts.limelightAngle) * (3.14 / 180);
-        double ydistance = FieldConsts.speakerAprilTagY - DeviceConsts.limelightHeight /Math.tan(yangleradians);
-        double xdistance = ydistance/Math.tan(xangle);
-        return new double[] {xdistance, ydistance};
-    }
+  public static Pose2d getAprilTagPose(){
+    return new Pose2d(getDistanceFromAngle(limelight)[0], getDistanceFromAngle(limelight)[1], new Rotation2d(limelight.getTx() * (3.14 / 180)));
+  }
 
-    public static Pose2d getAprilTagPose(){
-        return new Pose2d(getDistanceFromAngle(limelight)[0], getDistanceFromAngle(limelight)[1], new Rotation2d(limelight.getTx() * (3.14 / 180)));
-    }
+  @Override
+  public void execute() {
+      double yangle = limelight.getTy();
+      double xangle = limelight.getTx();
+      double ydistance = FieldConsts.speakerAprilTagY/Math.tan(yangle);
+      double xdistance = ydistance/Math.tan(xangle);
+      double yspeed = ydistance > 1 ? ydistance: ydistance > 15 ? 15 : 0;
+      double xspeed =  xdistance > 1 ? xdistance: xdistance > 15 ? 15 : 0;
+      Drivetrain.getInstance().driveRobotRelativeVelocity(new ChassisSpeeds(xspeed, yspeed, -xangle));
+      super.execute();
+  }
 
-    @Override
-    public void execute() {
-        double yangle = limelight.getTy();
-        double xangle = limelight.getTx();
-        double ydistance = FieldConsts.speakerAprilTagY/Math.tan(yangle);
-        double xdistance = ydistance/Math.tan(xangle);
-        double yspeed = ydistance > 1 ? ydistance: ydistance > 15 ? 15 : 0;
-        double xspeed =  xdistance > 1 ? xdistance: xdistance > 15 ? 15 : 0;
-        Drivetrain.getInstance().driveRobotRelativeVelocity(new ChassisSpeeds(xspeed, yspeed, -xangle));
-        super.execute();
-    }
+  @Override
+  public void cancel() {
+      super.cancel();
+  }
 
-    @Override
-    public void cancel() {
-        super.cancel();
-    }
+  @Override
+  public boolean isFinished() {
+      return super.isFinished();
+  }
 
-    @Override
-    public boolean isFinished() {
-        return super.isFinished();
-    }
-
-    @Override
-    public void end(boolean interrupted) {
-        super.end(interrupted);
-    }
+  @Override
+  public void end(boolean interrupted) {
+      super.end(interrupted);
+  }
 }
